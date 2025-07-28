2b156b852d8e7ac37e8655abb1efd15032f11a6c
StirlingSmith <stirlingjamessmith@gmail.com>
2025-07-28 14:53:48 +1200

    store model, and tweak model

--- train_NODE/main.jl
+++ train_NODE/main.jl
@@ -7,7 +7,6 @@ data = JSON.parsefile("./data/1_community_oscillation.json")
 data["L_series"][1]
 L_data = [[L[1] L[2]] for L in data["L_series"]] 
 
-L_data[2]
 u = hcat([reshape(L[:, :]', 10,1) for L in L_data]...)
 
 
@@ -20,7 +19,7 @@ tsteps = range(tspan[1], tspan[2]; length = datasize)
 
 ode_data = Array(u[:,1:15])
 
-dudt2 = Chain(x -> x, Dense(10, 100, tanh), Dense(100, 50, tanh), Dense(50, 10))
+dudt2 = Chain(x -> x, Dense(10, 100, tanh), Dense(100, 50, tanh), Dense(50, 50, tanh), Dense(50, 10))
 p, st = Lux.setup(rng, dudt2)
 prob_neuralode = NeuralODE(dudt2, tspan, Tsit5(); saveat = tsteps)
 
@@ -68,6 +67,5 @@ result_neuralode2 = Optimization.solve(
 callback((; u = result_neuralode2.u), loss_neuralode(result_neuralode2.u); doplot = true)
 
 using Serialization
-serialize("./models/1_community_oscillation/24-07-2025-untuned.jls", result_neuralode2.u)
+serialize("./models/1_community_oscillation/25-07-2025-L.jls", result_neuralode2.u)
 
-my_data_from_serialized = deserialize("./models/1_community_oscillation/24-07-2025-untuned.jls")
